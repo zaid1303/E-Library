56b418b3747d6ec69182ecf8961f2443feefb092
Zaid <zaidkh1303@gmail.com>
2024-03-03 06:31:55 +0530

    final commit

--- app.js
+++ app.js
@@ -9,9 +9,21 @@ const session = require("express-session");
 const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
 const fs=require('fs');
+const cors=require("cors");
 const { title } = require('process');
+const bookurl="https://www.googleapis.com/books/v1/volumes?q="
+const api_key="AIzaSyB2pnKdWYiokhfZCzAU1lsW4Y_TaT8JEw8";
+
 
 const app = express();
+app.use(cors(
+    {
+        origin:["https://deploy-mern-1whq.vercel.app"],
+        methods:["POST","GET"],
+        credentials: true
+    }
+))
+
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -70,26 +82,7 @@ var storage = multer.diskStorage({
 
 })
 var upload = multer({
-    storage: storage
-    // fileFilter:function(req,file,callback){
-    //     if(file.fieldname==='bookimage'){
-    //         if(file.mimetype==='image/jpg'||file.mimetype==='image/png'){
-    //             callback(null,true);
-    //         }
-    //         else{
-    //             callback(null,false);
-    //         }
-    //     }
-    //     if(file.fieldname==='bookfile'){
-    //         if(file.mimetype==='application/pdf'){
-    //             callback(null,true);
-    //         }
-    //         else{
-    //             callback(null,false);
-    //         }
-    //     }
-    // }
-});
+    storage: storage});
 
 app.get("/", (req, res) => {
     if (req.isAuthenticated()) {
@@ -146,13 +139,39 @@ app.get("/logout", function (req, res) {
     });
 })
 
+
+app.get("/book/view/:title",(req,res)=>{
+    const elementtitle=req.params.title;
+    Books.findOne({title:elementtitle}).then(function(foundbook){
+        var filePath ='./public/'+foundbook.file;
+        fs.readFile(filePath , function (err,data){
+            res.contentType("application/pdf");
+            res.send(data);
+        });
+    })
+})
+
+app.get("/book/download/:title",(req,res)=>{
+    const elementtitle=req.params.title;
+    Books.findOne({title:elementtitle}).then(function(foundbook){
+        res.download('./public/'+foundbook.file);
+    })
+})
+
+app.get("/upload", (req, res) => {
+    res.render("upload");
+})
+
+
 app.post("/register", (req, res) => {
     Users.register({ username: req.body.username }, req.body.password).then(function (user) {
         passport.authenticate("local")(req, res, function () {
-            res.redirect("/book");
+            res.redirect("/");
         })
     }).catch(function (err) {
-        console.log(err);
+        // popupS.alert({
+        //     content:"provide valid details"
+        // });
         res.redirect("/register");
     })
 })
@@ -209,27 +228,6 @@ app.post("/books",(req,res)=>{
     })
 })
 
-app.get("/book/view/:title",(req,res)=>{
-    const elementtitle=req.params.title;
-    Books.findOne({title:elementtitle}).then(function(foundbook){
-        var filePath ='./public/'+foundbook.file;
-        fs.readFile(filePath , function (err,data){
-            res.contentType("application/pdf");
-            res.send(data);
-        });
-    })
-})
-
-app.get("/book/download/:title",(req,res)=>{
-    const elementtitle=req.params.title;
-    Books.findOne({title:elementtitle}).then(function(foundbook){
-        res.download('./public/'+foundbook.file);
-    })
-})
-
-app.get("/upload", (req, res) => {
-    res.render("upload");
-})
 
 app.listen(process.env.PORT, function () {
     console.log(`server is running at ${process.env.PORT}.`);
